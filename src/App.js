29649824ba9d3a6a3072d4274a327b40d7c9d8d2
chaitanyakeerthiayinam <chaitanyakeerthiayinam@gmail.com>
2024-07-27 02:06:51 +0530

    added login and register components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
+import {AuthContext, AuthProvider} from "../src/context/AuthContext";
 import Menu from "./components/menu";
 import About from "./components/About";
 import Footer from "./components/footer";
 import Home from "./components/Home";
-import { Route,Routes } from "react-router-dom";
+import { Route,Routes, Navigate } from "react-router-dom";
 import VehicleList from "./components/VehicleList";
 import AddVehicle from "./components/AddVehicles";
 import UpdateVehicle from "./components/UpdateVehicle";
+import Login from './components/Login';
+import Register from './components/Register';
+
+
+import { useContext } from 'react';
+const ProtectedRoute = ({ element, ...rest }) => {
+  const { user } = useContext(AuthContext);
+
+  return user ? element : <Navigate to="/login" />;
+
+};
 
 
 function App(){
   return (
     <>
-    <Menu/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/vehicle-list" element={<VehicleList/>}/>
-      <Route path="/AddVehicles" element={<AddVehicle/>}/>
-      <Route path="/UpdateVehicle/:id" element = {<UpdateVehicle/>}/>
-      
-    </Routes>
-    <Footer/>
+    <AuthProvider>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vehicle-list" element={<VehicleList />} />
+        <Route path="/AddVehicles" element={<AddVehicle />} />
+        <Route path="/UpdateVehicle/:id" element={<UpdateVehicle />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+
+      </Routes>
+      <Footer />
+      </AuthProvider>
     </>
   )
 }
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,14 +1,18 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from 'react-router-dom';
 function Menu(){
-    return (
+  const { user, logout } = useContext(AuthContext);
+  return (
       <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">ABC Automobiles</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="container-fluid">
+        <Link className="navbar-brand" href="#">ABC Automobiles</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0" data-testid="menu-content">
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
@@ -16,17 +20,40 @@ function Menu(){
         <li className="nav-item">
           <Link className="nav-link"to="/about">About Us</Link>
         </li>
-        <li className="nav-item">
+        {user &&(
+          <>
+          <li className="nav-item">
           <Link className="nav-link" to="/Vehicle-List">Vehicles</Link>
-        </li>
+          </li>
+          </>)}
+        {user?.role === 'admin' && (  
+          <>
         <li className="nav-item">
           <Link className="nav-link" to="/AddVehicles">AddVehicles</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/UpdateVehicle">UpdateVehicle</Link>
         </li>
+        </>
+        )}
+        {!user && (
+        <>
         <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
+        <Link className="nav-link" to="/services">Services</Link>
+        </li>
+        <li className="nav-item">
+        <Link className="nav-link" to="/login">Login</Link>
+        </li>
+        <li className="nav-item">
+        <Link className="nav-link" to="/register">Register</Link>
+        </li>
+        </>
+        )}
+        {user && (
+        <button className="btn btn-outline-light" onClick={logout}>Logout</button>
+       )}
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Contact</Link>
         </li>
        
       </ul>
